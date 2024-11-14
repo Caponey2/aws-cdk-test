7304a938921e36327634fd36e6e742ac1faa95a8
Tanner <tannerhebert20xx@gmail.com>
2024-11-14 15:02:27 -0800

    Test of version incrementation

--- lambda/main.py
+++ lambda/main.py
@@ -3,7 +3,7 @@ import os
 def handler(event, context):
     version = os.environ.get("VERSION", "0.0")
     response_body = {
-        "message": "Hello my pre-commit hook??",
+        "message": "Hello my version1.1??",
         "version": version
     }
     return {"statusCode": 200, "body": response_body}
