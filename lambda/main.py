f8c82c3f29527c5fa056b8c48f71be467fd78c67
Tanner <tannerhebert20xx@gmail.com>
2024-11-14 14:44:02 -0800

    SHould be one more version

--- lambda/main.py
+++ lambda/main.py
@@ -3,7 +3,7 @@ import os
 def handler(event, context):
     version = os.environ.get("VERSION", "0.0")
     response_body = {
-        "message": "Hello my worldling",
+        "message": "Hello my v1.01?",
         "version": version
     }
     return {"statusCode": 200, "body": response_body}
