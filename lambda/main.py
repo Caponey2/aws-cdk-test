fc71541bf53860bebaedf6ca0b3fd7a83250e743
Tanner <tannerhebert20xx@gmail.com>
2024-11-14 15:00:19 -0800

    chmod +x for the pre-commit

--- lambda/main.py
+++ lambda/main.py
@@ -3,7 +3,7 @@ import os
 def handler(event, context):
     version = os.environ.get("VERSION", "0.0")
     response_body = {
-        "message": "Hello my v1.01?",
+        "message": "Hello my pre-commit hook??",
         "version": version
     }
     return {"statusCode": 200, "body": response_body}
